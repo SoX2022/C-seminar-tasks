9ee861bd57ad5c2ddf5d1485eb873ff3952d7f33
SoX <arksurviving5@gmail.com>
2022-09-30 23:20:37 +0300

    added ex 6

---
+++ Example_006/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter your number: ");
+string userNumber = Console.ReadLine() ?? "";
+int number = int.Parse(userNumber);
+
+Console.Clear();
+Console.Write(userNumber + " -> ");
+if (number % 2 == 0)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
