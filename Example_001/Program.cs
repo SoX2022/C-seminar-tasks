cf878118f5b1c783da0e4d21d6af8f36b107ef5f
SoX <arksurviving5@gmail.com>
2022-09-30 21:29:21 +0300

    added ex 1 and 3

---
+++ Example_001/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+string a = Console.ReadLine() ?? "";
+Console.Write("Введите второе число: ");
+string b = Console.ReadLine() ?? "";
+int a1 = int.Parse(a);
+int b1 = int.Parse(b);
+
+Console.Clear();
+Console.Write("a = "+ a + ", b = " + b + " -> ");
+if (a1 == b1*b1)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
---
+++ Example_003/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Введите номер дня недели: ");
+string day = Console.ReadLine() ?? "";
+int dayNumber = int.Parse(day);
+
+Console.Clear();
+Console.Write(day + " -> ");
+
+if (dayNumber - 7 * (dayNumber / 7) == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (dayNumber - 7 * (dayNumber / 7) == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (dayNumber - 7 * (dayNumber / 7) == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (dayNumber - 7 * (dayNumber / 7) == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (dayNumber - 7 * (dayNumber / 7) == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (dayNumber - 7 * (dayNumber / 7) == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (dayNumber - 7 * (dayNumber / 7) == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
