7f6e484cd7828b64ca6af64093677ba42e69809d
SoX <arksurviving5@gmail.com>
2022-09-30 23:49:05 +0300

    added ex 8

---
+++ Example_008/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter your number: ");
+string userNumber = Console.ReadLine() ?? "";
+int number = int.Parse(userNumber);
+int temp = 2;
+
+Console.Clear();
+Console.Write(userNumber + " -> ");
+
+while (temp < number - 1)
+{
+    Console.Write(temp + ", ");
+        temp += 2;
+}
+if (temp <= number)
+{
+    Console.WriteLine(temp);
+}
