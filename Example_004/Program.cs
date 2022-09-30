1d72ed7b6b1a096598b73598bb2fe6ae5f394f2f
SoX <arksurviving5@gmail.com>
2022-09-30 23:29:41 +0300

    added ex 4

---
+++ Example_004/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Enter first number: ");
+string userFirstNumber = Console.ReadLine() ?? "";
+Console.Write("Enter first number: ");
+string userSecondNumber = Console.ReadLine() ?? "";
+Console.Write("Enter first number: ");
+string userThirdNumber = Console.ReadLine() ?? "";
+int firstNumber = int.Parse(userFirstNumber);
+int secondNumber = int.Parse(userSecondNumber);
+int thirdNumber = int.Parse(userThirdNumber);
+int max = firstNumber;
+
+Console.Clear();
+Console.Write(userFirstNumber + ", " + userSecondNumber + ", " + userThirdNumber + " -> ");
+
+if (secondNumber > max)
+{
+    max = secondNumber;
+}
+if (thirdNumber > max)
+{
+    max = thirdNumber;
+}
+
+Console.WriteLine(max);
+
+
