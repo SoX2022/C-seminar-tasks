76fdcdd7fba5b0a3ab17b9641a6a406827ab82e6
SoX <arksurviving5@gmail.com>
2022-10-04 21:02:54 +0300

    added Example 009

---
+++ Example_009/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = new Random().Next(10, 100);
+int number1 = number / 10;
+int number2 = number % 10;
+
+//if (number1 < number2)
+//    number1 = number2;
+//
+//Console.WriteLine($"{number} -> {number1}");
+
+int max = number1;
+
+if (max < number2)
+    max = number2;
+
+Console.WriteLine($"{number} -> {max}");
