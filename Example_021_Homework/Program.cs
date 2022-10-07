6b63533d4ff651133874ea2f848a4952fd0322f1
SoX <arksurviving5@gmail.com>
2022-10-08 01:10:43 +0300

    added Example 021 Homework and Example 023 Homework

---
+++ Example_021_Homework/Program.cs
@@ -0,0 +1,61 @@
+﻿//Напигите программу, которая принимает на вход коордтнаты двух точек и находит расстояние между ними в 3D пространстве.
+//A(3,6,8); B(2,1,-7) -> 15.84
+//A(7,-5,0); B(1,-1,9) -> 11.53
+
+
+static void Enter3dCoordinates(int[] array)
+{
+    string[] vector = { "X", "Y", "Z" };
+    int lenght = array.Length;
+    int index = 0;
+
+    while (index < lenght)
+    {
+        while (true)
+        {
+            Console.Write($"{vector[index]} = ");
+            if (int.TryParse(Console.ReadLine() ?? "", out array[index]))
+                break;
+            else
+                Console.WriteLine("Invalid value. Please, try again.");
+        }
+        index++;
+    }
+}
+
+static void Print3dCoordinates(int[] array, string message)
+{
+    int lenght = array.Length;
+    int index = 0;
+
+    Console.Write($"{message}(");
+    while (index < lenght - 1)
+    {
+        Console.Write($"{array[index]},");
+        index++;
+    }
+    Console.Write($"{array[index]})");
+}
+
+static double DistanseCalculator3D(int[] array1, int[] array2)
+{
+    double distanse = Math.Round(Math.Sqrt(Math.Pow(array2[0] - array1[0], 2) + Math.Pow(array2[1] - array1[1], 2) + Math.Pow(array2[2] - array1[2], 2)), 2);
+    return distanse;
+}
+
+int[] A = new int[3];
+int[] B = new int[3];
+
+Console.Clear();
+Console.WriteLine("Enter first coordinates, please.");
+Enter3dCoordinates(A);
+Console.WriteLine("Enter second coordinates, please.");
+Enter3dCoordinates(B);
+
+double distanse = DistanseCalculator3D(A, B);
+
+Console.Clear();
+Print3dCoordinates(A, "A");
+Console.Write("; ");
+Print3dCoordinates(B, "B");
+Console.WriteLine($" ->  {distanse}");
---
+++ Example_023_Homework/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, которая принимает на вход число N и выдает таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27.
+//5 -> 1, 8, 27, 64, 125.
+
+static void PrintSquares(int N)
+{
+    for (int i = 1; i < N; i++)
+    {
+        Console.Write($"{Math.Pow(i, 3)}, ");
+    }
+}
+
+
+int number;
+
+
+while (true)
+{
+    Console.Write("Введите число: ");
+    if (int.TryParse(Console.ReadLine() ?? "", out number) && number > 0)
+        break;
+    else
+        Console.WriteLine("Invalid value. Please, try again.");
+}
+
+Console.Clear();
+Console.Write($"{number} -> ");
+PrintSquares(number);
+Console.WriteLine($"{Math.Pow(number, 3)}.");
+
