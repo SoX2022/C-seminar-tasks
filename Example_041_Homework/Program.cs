b6abd3b62798ab7278aa45494def0a78a5234021
SoX <arksurviving5@gmail.com>
2022-10-18 22:41:24 +0300

    added homework 41 and 43

---
+++ Example_041_Homework/Program.cs
@@ -0,0 +1,51 @@
+﻿//Пользователь вводит с клавиатуры N чисел.
+//Посчитайте, сколько чисел больше 0 ввел пользователь.
+//
+//0,7,8,-2,-2 -> 2
+//-1,-7,567,89,223 -> 3
+
+
+static int[] FillArray(int[] array)
+{
+    int index = 0;
+    Console.WriteLine("Enter array data. To stop enter 'Q'");
+
+    while (true)
+    {
+        string userArrayData = Console.ReadLine() ?? "";
+        if (int.TryParse(userArrayData, out int userData))
+        {
+            Array.Resize(ref array, index + 1);
+            array[index] = userData;
+            index++;
+        }
+        else
+        {
+            userArrayData = userArrayData.ToLower();
+            if (userArrayData == "q")
+                return array;
+            else
+                Console.WriteLine("Error");
+        }
+    }
+}
+
+static int PositiveValueCounter(int[] array)
+{
+    int result = 0;
+    foreach (int element in array)
+    {
+        if (element > 0)
+            result++;
+    }
+    return result;
+}
+
+
+Console.Clear();
+int[] userArray = new int[0];
+userArray = FillArray(userArray);
+int counter = PositiveValueCounter(userArray);
+Console.Clear();
+Console.Write(String.Join(",", userArray));
+Console.WriteLine($" -> {counter}");
---
+++ Example_043_Homework/Program.cs
@@ -0,0 +1,44 @@
+﻿//Напишите программу, которая найдет точку пересечения двух прямых, заданных уравнениями
+//y = k1 * x + b1
+//y = k2 * x + b2
+//значения b1, k1, b2, k2 задаются пользователем.
+//
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+static int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine() ?? "", out int userNumber );
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+static double GetXKoordinate(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber)
+{
+    double x = (double) (thirdNumber - firstNumber) / (secondNumber- fourthNumber);
+    return x;
+}
+
+static double GetYKoordinate(int firstNumber, int secondNumber, double xKoordinate)
+{
+    double y = secondNumber * xKoordinate + firstNumber;
+    return y;
+}
+
+
+Console.Clear();
+int userNumberB1 = GetNumberFromUser("Enter b1 : ", "Error. Try again.");
+int userNumberK1 = GetNumberFromUser("Enter k1 : ", "Error. Try again.");
+int userNumberB2 = GetNumberFromUser("Enter b2 : ", "Error. Try again.");
+int userNumberK2 = GetNumberFromUser("Enter k2 : ", "Error. Try again.");
+
+double x = GetXKoordinate(userNumberB1, userNumberK1, userNumberB2, userNumberK2);
+double y = GetYKoordinate(userNumberB1, userNumberK1, x);
+
+Console.Clear();
+Console.Write($"b1 = {userNumberB1}, k1 = {userNumberK1}, b2 = {userNumberB2}, k2 = {userNumberK2} -> ({x};{y})");
